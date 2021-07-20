c5f5998ff1298d741f33f3b2ea3a68ed87e55b98
Tirtharaj Sur <tirtharajsur@gmail.com>
2021-07-20 13:14:22 +0530

    Added gitignore File

--- static/js/ide.js
+++ static/js/ide.js
@@ -3,7 +3,8 @@ let editor;
 window.onload = function() {
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/c_cpp");
+    // editor.session.setMode("ace/mode/c_cpp");
+    editor.session.setMode("ace/mode/python");
 }
 
 function changeLanguage() {
@@ -11,7 +12,7 @@ function changeLanguage() {
     let language = $("#languages").val();
     if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
     else if(language == 'java')editor.session.setMode("ace/mode/java");
-    else if(language == 'python')editor.session.setMode("ace/mode/python");
+    else if(language == 'py')editor.session.setMode("ace/mode/python");
 }
 
 function executeCode() {

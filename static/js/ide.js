7986f4a34f141c1e8927dbcd1c708822f1bc52ef
Manish <mr.manish22d@gmail.com>
2021-07-21 11:13:17 +0530

    updated code : Manish

--- static/js/ide.js
+++ static/js/ide.js
@@ -1,36 +1,33 @@
 let editor;
 
-window.onload = function() {
-    editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    // editor.session.setMode("ace/mode/c_cpp");
-    editor.session.setMode("ace/mode/python");
-}
+window.onload = function () {
+  editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  // editor.session.setMode("ace/mode/c_cpp");
+  editor.session.setMode("ace/mode/python");
+};
 
 function changeLanguage() {
-
-    let language = $("#languages").val();
-    if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
-    else if(language == 'java')editor.session.setMode("ace/mode/java");
-    else if(language == 'py')editor.session.setMode("ace/mode/python");
+  let language = $("#languages").val();
+  if (language == "c" || language == "cpp")
+    editor.session.setMode("ace/mode/c_cpp");
+  else if (language == "java") editor.session.setMode("ace/mode/java");
+  else if (language == "py") editor.session.setMode("ace/mode/python");
 }
 
 function executeCode() {
+  $.ajax({
+    url: "/excecuteCode",
+    method: "GET",
 
-    $.ajax({
+    data: {
+      language: $("#languages").val(),
+      code: editor.getSession().getValue(),
+      csrfViewMiddleware: $("input[name=csrfmiddlewaretoken").val(),
+    },
 
-        url: "/excecuteCode",
-        method: "GET",
-
-        data: {
-            language: $("#languages").val(),
-            code: editor.getSession().getValue(),
-            csrfViewMiddleware:$('input[name=csrfmiddlewaretoken').val(),
- 
-        },
-
-        success: function(response) {
-            $(".output").text(response)
-        }
-    })
+    success: function (response) {
+      $(".output").text(response);
+    },
+  });
 }
